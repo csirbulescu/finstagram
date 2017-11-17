d341dd4b9f36bdd9ae141d94b945df0a59e0357c
Corina Sirbulescu <csirbulescu@gmail.com>
2017-11-17 04:33:52 +0000

    fixed humanized time ago

--- app/actions.rb
+++ app/actions.rb
@@ -1,10 +1,18 @@
+def humanized_time_ago(time_ago_in_minutes)
+  if time_ago_in_minutes >= 60
+    "#{time_ago_in_minutes / 60} hours ago"
+  else
+    "#{time_ago_in_minutes} minutes ago"
+  end
+end
+
 get '/' do
     
    @post_shark = {
        username: "sharky_j",
        avatar_url: "http://naserca.com/images/sharky_j.jpg",
        photo_url: "http://naserca.com/images/shark.jpg",
-       humanized_time_ago: 15,
+       humanized_time_ago: humanized_time_ago(15),
        like_count: 0,
        comment_count: 1,
        comments: [{
@@ -17,7 +25,7 @@ get '/' do
      username: "kirk_whalum",
      avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
      photo_url: "http://naserca.com/images/whale.jpg",
-       humanized_time_ago: 65,
+       humanized_time_ago: humanized_time_ago(65),
        like_count: 0,
        comment_count: 1,
        comments: [{
@@ -30,7 +38,7 @@ get '/' do
      username: "marlin_peppa",
      avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
      photo_url: "http://naserca.com/images/marlin.jpg",
-       humanized_time_ago: 190,
+       humanized_time_ago: humanized_time_ago(190),
        like_count: 0,
        comment_count: 1,
        comments: [{
