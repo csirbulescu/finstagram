5a527901f7411b1c2c5d5dc31b12d1c73a3f5065
Corina Sirbulescu <csirbulescu@gmail.com>
2017-12-06 02:35:16 +0000

    added login stuff and things

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,60 @@
+helpers do
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+end
+
 get '/' do
     @posts = Post.order(created_at: :desc)
     erb(:index)
+end 
+
+get '/signup' do
+    @user = User.new
+    erb(:signup)
 end
+
+post '/signup' do
+    #grab user input values from params
+    email = params[:email]
+    avatar_url = params[:avatar_url]
+    username = params[:username]
+    password = params[:password]
+    
+  
+    #instantiate and save user
+    @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+    
+    if @user.save
+        redirect to('/login')
+    else
+        erb(:signup)
+    end
+end 
+    
+
+
+get '/login' do
+    erb(:login)
+end
+
+post '/login' do
+    username = params[:username]
+    password = params[:password]
+    
+    user = User.find_by(username: username)
+    
+    if user && user.password == password
+        session[:user_id] = user.id
+        redirect to('/')
+        "Success! User with id #{session[:user_id]} is logged in!"
+     else 
+         "Login failed"
+     end
+end 
+
+get '/logout' do
+    session[:user_id] = nil
+    redirect to('/')
+end
+
